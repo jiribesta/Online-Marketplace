32eb40de159740a89b9ef224c55c818d80dd0a2b
Jiří Bešta <jiribesta@protonmail.com>
2025-09-27 16:10:47 +0200

    Add logging setup file

---
+++ src/app_config.py
@@ -0,0 +1,32 @@
+import json
+import os
+import sys
+
+CONFIG_FILE_DIR = "../"
+CONFIG_FILE_NAME = "config.json"
+
+def load_config():
+    try:
+        with open(os.path.join(CONFIG_FILE_DIR, CONFIG_FILE_NAME), "r") as config_file:
+            return json.load(config_file)
+    except Exception as e:
+        print(f"Unexpected error when obtaining config data: {e}")
+        sys.exit(1)
+
+def get_abs_or_rel_path(file_path):
+    if os.path.isabs(file_path):
+        return file_path
+    else:
+        return os.path.join(CONFIG_FILE_DIR, file_path)
+
+config = load_config()
+
+DB_USERNAME = config.get("database", {}).get("db_username", "postgres")
+DB_PASSWORD = config.get("database", {}).get("db_password", "password123")
+DB_HOST = config.get("database", {}).get("db_host", "localhost")
+DB_PORT = config.get("database", {}).get("db_port", 5432)
+DB_NAME = config.get("database", {}).get("db_name", "marketplace")
+
+LOG_FILE_PATH = get_abs_or_rel_path(config.get("log_file_path", "./log.log"))
+
+IMAGES_FOLDER_PATH = get_abs_or_rel_path(config.get("images_folder_path", "./images"))
---
+++ src/logging_config.py
@@ -0,0 +1,22 @@
+import logging
+
+from app_config import LOG_FILE_PATH
+
+logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler(LOG_FILE_PATH)
+console_handler = logging.StreamHandler()
+
+file_handler.setLevel(logging.DEBUG)  # Log all messages to the file
+console_handler.setLevel(logging.WARNING)  # Log warnings and worse to the console
+
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_formatter = logging.Formatter('%(levelname)s - %(message)s')
+
+file_handler.setFormatter(file_formatter)
+console_handler.setFormatter(console_formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
